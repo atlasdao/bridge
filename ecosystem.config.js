ea1b8ae911e778dcef1a55cd24f46e1ba5ccbe2d
Atlas DAO <215057932+atlasdao@users.noreply.github.com>
2025-08-04 02:47:16 +0000

    Dev environment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,18 @@
 module.exports = {
   apps : [{
-    name   : "atlas-bridge-PROD", // Pode dar um nome novo/distinto para clareza
-    script : "src/app.js",       // Caminho relativo ao cwd, entao 'src/app.js' esta OK
-    cwd    : "/opt/bridge_app/main/", // <<< caminho de atuacao do pm2
-    watch  : false,
-    max_memory_restart: "250M",
+    name   : "atlas-bridge-prod",
+    script : "./src/app.js",
     env_production: {
-       NODE_ENV: "production",
-       PORT: 3000 // Porta de producao
-    },
-    // Atualize os caminhos dos logs para serem relativos ao novo CWD ou absolutos
-    out_file : "./logs/pm2-out.log",  // Relativo a /opt/bridge_app/main/
-    error_file : "./logs/pm2-err.log", // Relativo a /opt/bridge_app/main/
-    // OU caminhos absolutos:
-    // out_file : "/opt/bridge_app/main/logs/pm2-out.log",
-    // error_file : "/opt/bridge_app/main/logs/pm2-err.log",
-    log_date_format : "YYYY-MM-DD HH:mm:ss Z",
-    merge_logs : true,
+      NODE_ENV: "production"
+    }
+  }, {
+    name   : "atlas-bridge-dev",
+    script : "./src/app.js",
+    watch: ["src"],
+    watch_delay: 1000,
+    ignore_watch : ["node_modules"],
+    env_development: {
+       NODE_ENV: "development"
+    }
   }]
 }
